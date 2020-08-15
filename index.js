c8f21b1b369149e20e63044bbdea3621a369de5f
michael-small <small203@morris.umn.edu>
2020-08-15 15:03:23 -0500

    Link express to react routes

--- index.js
+++ index.js
@@ -23,5 +23,13 @@ const app = express();
 
 // require('./routes/authRoutes')(app); //require returns a function, so app is its arg
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
