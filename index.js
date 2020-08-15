37b6bfe8af2bbe875e202f9fde9a6ab0beacc5c3
michael-small <small203@morris.umn.edu>
2020-08-15 14:56:58 -0500

    Hide auth

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
+// const mongoose = require('mongoose');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
+// const keys = require('./config/keys');
 
-require('./models/User');
-require('./services/passport');
+// require('./models/User');
+// require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+// mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in ms
-    keys: [keys.cookieKey],
-  })
-);
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in ms
+//     keys: [keys.cookieKey],
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-require('./routes/authRoutes')(app); //require returns a function, so app is its arg
+// require('./routes/authRoutes')(app); //require returns a function, so app is its arg
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
