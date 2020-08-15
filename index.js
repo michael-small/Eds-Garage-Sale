29cd8378da54d3b33bddf3b07f42c64c745a090a
michael-small <small203@morris.umn.edu>
2020-08-15 15:26:52 -0500

    Use env paths like lecture

--- index.js
+++ index.js
@@ -24,13 +24,18 @@ const app = express();
 
 // require('./routes/authRoutes')(app); //require returns a function, so app is its arg
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
