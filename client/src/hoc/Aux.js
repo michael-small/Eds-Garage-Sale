39d7f2be601374ecbf86aa58bf46d0cbd6181938
michael-small <small203@morris.umn.edu>
2020-08-17 11:29:56 -0500

    Add `Aux.js` wrapper in `hoc` folder (high order component)

---
+++ client/src/hoc/Aux.js
@@ -0,0 +1,7 @@
+// empty wrapper for child elements
+// used when we want adjacent elements without extra wrapper element in real DOM
+const aux = (props) => props.children;
+
+// React 16.2 uses React.Fragment
+
+export default aux;
