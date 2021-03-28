ea97227ecf14d28ab3e622ded8f3f0793569ecce
michael-small <small203@morris.umn.edu>
2021-03-28 17:33:53 -0500

    Convert CCs to FCs

--- client/src/components/Cockpit/Listings/Listing/Title/Title.js
+++ client/src/components/Cockpit/Listings/Listing/Title/Title.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Title.css';
 
-class Title extends Component {
-  render() {
-    return (
-      <h4 className='Title'>
-        {this.props.item}
-        {this.props.currentSearch !== '' && (
-          <span>
-            <a
-              href={this.props.currentSearch}
-              style={{ textDecoration: 'none' }}
-            >
-              {' '}
-              🛒
-            </a>
-          </span>
-        )}
-        {this.props.soldSearch !== '' && (
-          <span>
-            <a href={this.props.soldSearch} style={{ textDecoration: 'none' }}>
-              💰
-            </a>
-          </span>
-        )}
-      </h4>
-    );
-  }
+export default function Title(props) {
+	return (
+		<h4 className='Title'>
+			{props.item}
+			{props.currentSearch !== '' && (
+				<span>
+					<a
+						href={props.currentSearch}
+						style={{ textDecoration: 'none' }}
+					>
+						{' '}
+						🛒
+					</a>
+				</span>
+			)}
+			{props.soldSearch !== '' && (
+				<span>
+					<a
+						href={props.soldSearch}
+						style={{ textDecoration: 'none' }}
+					>
+						💰
+					</a>
+				</span>
+			)}
+		</h4>
+	);
 }
-
-export default Title;
--- client/src/components/Cockpit/Listings/Listings.js
+++ client/src/components/Cockpit/Listings/Listings.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Listing from './Listing/Listing';
 
-class Listings extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		let filteredComics = this.props.comics.filter((comic) => {
-			return (
-				comic.gsx$item.$t
-					.toLowerCase()
-					.indexOf(this.props.search.toLowerCase()) !== -1
-			);
-		});
-
+export default function Listings(props) {
+	const filteredComics = props.comics.filter((comic) => {
 		return (
-			<Aux>
-				{filteredComics.reverse().map((comic, index) => (
-					<Listing listing={comic} keyVal={comic.id.$t} key={index} />
-				))}
-			</Aux>
+			comic.gsx$item.$t
+				.toLowerCase()
+				.indexOf(props.search.toLowerCase()) !== -1
 		);
-	}
-}
+	});
 
-export default Listings;
+	return (
+		<Aux>
+			{filteredComics.reverse().map((comic, index) => (
+				<Listing listing={comic} keyVal={comic.id.$t} key={index} />
+			))}
+		</Aux>
+	);
+}
