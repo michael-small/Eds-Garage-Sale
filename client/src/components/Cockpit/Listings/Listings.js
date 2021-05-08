6b3bbfba5a1bdef036ed0b4ef14f57b70702e8e7
michael-small <small203@morris.umn.edu>
2021-05-08 13:40:04 -0500

    Break listings into individual component each
    Addresses #14 (Move listings view from map on `Listings FC` to individual `Listing FCs`)

--- client/src/components/Cockpit/Listings/Listings.js
+++ client/src/components/Cockpit/Listings/Listings.js
@@ -1,40 +1,14 @@
 import React from 'react';
-import './Listings.css';
-import Aux from '../../../hoc/Aux';
-// import Listing from './Listing/Listing';
-import PublishPhoto from '../../../images/publish_photo.jpg';
+import Listing from './Listing/Listing';
 
 // MUI Components
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Chip from '@material-ui/core/Chip';
-import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
+
 import clsx from 'clsx';
+import './Listings.css';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		display: 'flex',
-		justifyContent: 'center',
-		flexWrap: 'wrap',
-		'& > *': {
-			margin: theme.spacing(0.5),
-		},
-	},
-	themeColor: {
-		backgroundColor: theme.palette.primary.main,
-		padding: '8px',
-	},
-	themeText: {
-		color: theme.palette.primary.contrastText,
-	},
-	themeSecondary: {
-		backgroundColor: theme.palette.secondary.main,
-		color: theme.palette.secondary.contrastText,
-	},
 	grid: {
 		marginBottom: '8px',
 	},
@@ -62,54 +36,7 @@ export default function Listings(props) {
 						key={index}
 						align='center'
 					>
-						<Card
-							className={clsx('card', classes.themeColor)}
-							style={{ height: '100%' }}
-						>
-							{comic.photo !== null ? (
-								<CardMedia
-									className='card-img'
-									image={comic.photo.url}
-								/>
-							) : (
-								<CardMedia
-									className='card-img'
-									image={PublishPhoto}
-								/>
-							)}
-
-							<CardContent>
-								<Typography variant='h5'>
-									{comic.name}
-								</Typography>
-								<Typography color='textSecondary' variant='h6'>
-									${comic.price}
-								</Typography>
-								{comic.shortDescription ? (
-									<Typography variant='subtitle1'>
-										{comic.shortDescription}
-									</Typography>
-								) : (
-									<Aux>
-										<br />
-										<Typography variant='caption'>
-											No description
-										</Typography>
-									</Aux>
-								)}
-								<div key={index} className={classes.root}>
-									{comic.categories.map(({ name }, index) => (
-										<Chip
-											key={index}
-											label={name}
-											color='primary'
-											size='medium'
-											className={classes.themeSecondary}
-										/>
-									))}
-								</div>
-							</CardContent>
-						</Card>
+						<Listing listing={comic} />
 					</Grid>
 				))}
 			</Grid>
